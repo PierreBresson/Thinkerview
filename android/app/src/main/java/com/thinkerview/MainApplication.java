41d5ff885881965a92fd6dd741797bf851d1216d
Pierre Bresson <hi@pierrebresson.com>
2018-08-05 12:27:30 +0200

    remove duplicates

--- android/app/src/main/java/com/thinkerview/MainApplication.java
+++ android/app/src/main/java/com/thinkerview/MainApplication.java
@@ -3,11 +3,7 @@ package com.thinkerview;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.oblador.vectoricons.VectorIconsPackage;
-import com.inprogress.reactnativeyoutube.ReactNativeYouTube;
-import com.oblador.vectoricons.VectorIconsPackage;
 import guichaguri.trackplayer.TrackPlayer;
-import cl.json.RNSharePackage;
 import com.rpt.reactnativecheckpackageinstallation.CheckPackageInstallationPackage;
 import cl.json.RNSharePackage;
 import com.oblador.vectoricons.VectorIconsPackage;
@@ -31,8 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
 
     @Override
     protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(new MainReactPackage(),
-            new VectorIconsPackage(), new TrackPlayer(), new RNSharePackage(),
+      return Arrays.<ReactPackage>asList(new MainReactPackage(), new TrackPlayer(), new RNSharePackage(),
           new VectorIconsPackage(), new CheckPackageInstallationPackage(), new ReactNativeYouTube());
     }
 
