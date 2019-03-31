976dedb6cb7cd55e757bec85c7999a75bd0e0f13
Pierre Bresson <PierreBresson@users.noreply.github.com>
2019-03-31 17:13:00 +0200

    update to rn59

--- ios/thinkerview/AppDelegate.m
+++ ios/thinkerview/AppDelegate.m
@@ -23,7 +23,11 @@
 {
   NSURL *jsCodeLocation;
 
-  jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
+  #ifdef DEBUG
+    jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
+  #else
+    jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+  #endif
 
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"thinkerview"
@@ -38,7 +42,7 @@
   [self.window makeKeyAndVisible];
   
   [[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryPlayback error: nil];
-  
+
   return YES;
 }
 
--- ios/thinkerview/doNotDeleteThisDummyFile.swift
+++
@@ -1,9 +0,0 @@
-//
-//  doNotDeleteThisDummyFile.swift
-//  thinkerview
-//
-//  Created by Pierre Bresson on 15/07/2018.
-//  Copyright © 2018 Facebook. All rights reserved.
-//
-
-import Foundation
